d86c803750a878a16327285c85c2a442f6204942
zetetic <>
2021-03-03 18:54:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+//let diyMacro = new Macro.create({name:"XX"});
+Macro.create({
+    name: 'XXX',
+    command: "some Thing",
+    img: "icons/svg/dice-target.svg",
+    permission: {
+        default: 3
+    },
+    scope: "global",
+    sort: 100001,
+    type: "script"
+})
+//game.user.assignHotbarMacro(diyMacro, 4);
 let a = game.macros.filter((m) => true)[0];
 //a.name='23'
-console.log('game.macros', a)
-
-game.user.assignHotbarMacro(a, 4);
-
-//game.macros.insert(a)
+console.log('game.macros AAAAAAAA', game.macros.filter((m) => true))
+//game.user.assignHotbarMacro(diyMacro, 4);
